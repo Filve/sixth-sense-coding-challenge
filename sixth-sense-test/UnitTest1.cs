39aabe19b5a2c8ecb8d311d42fa65da47582ac04
Francesco Simeoni <fravale@live.it>
2021-11-24 23:16:57 +0100

    initial commit

---
+++ sixth-sense-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace sixth_sense_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
