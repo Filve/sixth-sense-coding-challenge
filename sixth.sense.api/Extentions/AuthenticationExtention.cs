9a2daec95a0018de4b458ad12cb7b9a6d2712536
Francesco Simeoni <fravale@live.it>
2021-11-28 17:15:10 +0100

    implement authentication extention

---
+++ sixth.sense.api/Extentions/AuthenticationExtention.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace sixth.sense.api.Extentions
+{
+    public class AuthenticationExtention : Startup
+    {
+        public AuthenticationExtention(IConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
